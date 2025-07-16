a1cb3dd4a375805951ce78a559ac3c66bb3491b0
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2025-07-16 09:09:54 -0500

    Handle requesting nonexistent documents

--- cms.rb
+++ cms.rb
@@ -4,15 +4,27 @@ require 'tilt/erubi'
 
 root = File.expand_path('..', __FILE__)
 
-get '/' do
-  @files = Dir.glob('*', base: File.join(root, 'data'))
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
 
+before do
+  @files = Dir.glob('*', base: File.join(root, 'data'))
+end
+
+get '/' do
   erb :index
 end
 
 get '/:file_name' do
   file = params[:file_name]
   file_path = "#{root}/data/#{file}"
+
+  unless File.file?(file_path)
+    session[:error] = "#{file} does not exist."
+    redirect '/'
+  end
 
   headers['Content-Type'] = 'text/plain'
   File.read(file_path)
---
+++ test/cms_test.rb
@@ -0,0 +1,48 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../cms'
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get '/'
+
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_includes last_response.body, 'about.txt'
+    assert_includes last_response.body, 'changes.txt'
+    assert_includes last_response.body, 'history.txt'
+  end
+
+  def test_viewing_text_document
+    get '/history.txt'
+
+    assert_equal 200, last_response.status
+    assert_equal 'text/plain', last_response['Content-Type']
+    assert_includes last_response.body, '2022 - Ruby 3.2 released.'
+  end
+
+  def test_viewing_nonexistent_document
+    get '/notafile.ext'
+
+    assert_equal 302, last_response.status
+    assert_empty last_response.body
+
+    get last_response['Location']
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'notafile.ext does not exist.'
+
+    get '/'
+
+    refute_includes last_response.body, 'notafile.ext does not exist.'
+  end
+end
